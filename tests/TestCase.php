f3867629ca912f3eece12d9110d0113e8fa3005f
Josh <josh@bonnick.dev>
2024-05-29 15:42:31 +0100

    pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function starWarsRoute(string $uri): string
-    {
-        return config('swapi.base_url').$uri;
-    }
+    use CreatesApplication;
 }
